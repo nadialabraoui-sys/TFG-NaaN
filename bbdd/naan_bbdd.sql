acf6f825298035f3fc91baad7ccdc8062109cfd6
nadialabraoui-sys <nadialabraoui@gmail.com>
2026-02-21 12:56:45 +0100

    Add files via upload

---
+++ bbdd/naan_bbdd.sql
@@ -0,0 +1,539 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 21-02-2026 a las 11:55:20
+-- Versión del servidor: 8.0.44
+-- Versión de PHP: 8.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `naan_bbdd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auditoria`
+--
+
+CREATE TABLE `auditoria` (
+  `id_auditoria` int NOT NULL,
+  `tabla_afectada` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_registro` int DEFAULT NULL,
+  `operacion` enum('INSERT','UPDATE','DELETE') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `datos_anteriores` json DEFAULT NULL,
+  `datos_nuevos` json DEFAULT NULL,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `id_carrito` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `fecha_creacion` datetime DEFAULT CURRENT_TIMESTAMP,
+  `total` decimal(10,2) DEFAULT '0.00',
+  `estado` enum('activo','comprado') COLLATE utf8mb4_unicode_ci DEFAULT 'activo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_carrito`
+--
+
+CREATE TABLE `detalle_carrito` (
+  `id_detalle` int NOT NULL,
+  `id_carrito` int NOT NULL,
+  `id_producto` int NOT NULL,
+  `cantidad` int NOT NULL,
+  `precio_unitario` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Disparadores `detalle_carrito`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_carrito_total_delete` AFTER DELETE ON `detalle_carrito` FOR EACH ROW BEGIN
+    UPDATE Carrito
+    SET total = (
+        SELECT COALESCE(SUM(cantidad * precio_unitario),0)
+        FROM Detalle_Carrito
+        WHERE id_carrito = OLD.id_carrito
+    )
+    WHERE id_carrito = OLD.id_carrito;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `trg_carrito_total_insert` AFTER INSERT ON `detalle_carrito` FOR EACH ROW BEGIN
+    UPDATE Carrito
+    SET total = (
+        SELECT COALESCE(SUM(cantidad * precio_unitario),0)
+        FROM Detalle_Carrito
+        WHERE id_carrito = NEW.id_carrito
+    )
+    WHERE id_carrito = NEW.id_carrito;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `trg_carrito_total_update` AFTER UPDATE ON `detalle_carrito` FOR EACH ROW BEGIN
+    UPDATE Carrito
+    SET total = (
+        SELECT COALESCE(SUM(cantidad * precio_unitario),0)
+        FROM Detalle_Carrito
+        WHERE id_carrito = NEW.id_carrito
+    )
+    WHERE id_carrito = NEW.id_carrito;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_pedido`
+--
+
+CREATE TABLE `detalle_pedido` (
+  `id_detalle_pedido` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `id_producto` int NOT NULL,
+  `cantidad` int NOT NULL,
+  `precio_en_el_momento` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direccion`
+--
+
+CREATE TABLE `direccion` (
+  `id_direccion` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `calle` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `numero` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ciudad` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `provincia` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `codigo_postal` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pais` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favorito`
+--
+
+CREATE TABLE `favorito` (
+  `id_usuario` int NOT NULL,
+  `id_producto` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_estado_pedido`
+--
+
+CREATE TABLE `historial_estado_pedido` (
+  `id_historial` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `estado` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `comentario` text COLLATE utf8mb4_unicode_ci,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `metodo_pago`
+--
+
+CREATE TABLE `metodo_pago` (
+  `id_metodo_pago` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `tipo` enum('paypal','tarjeta','bizum','google_pay') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `numero_enmascarado` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `es_predeterminado` tinyint(1) DEFAULT '0',
+  `fecha_expiracion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Disparadores `metodo_pago`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_metodo_predeterminado_insert` BEFORE INSERT ON `metodo_pago` FOR EACH ROW BEGIN
+    IF NEW.es_predeterminado = TRUE THEN
+        UPDATE Metodo_Pago
+        SET es_predeterminado = FALSE
+        WHERE id_usuario = NEW.id_usuario;
+    END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `trg_metodo_predeterminado_update` BEFORE UPDATE ON `metodo_pago` FOR EACH ROW BEGIN
+    IF NEW.es_predeterminado = TRUE THEN
+        UPDATE Metodo_Pago
+        SET es_predeterminado = FALSE
+        WHERE id_usuario = NEW.id_usuario
+        AND id_metodo_pago <> NEW.id_metodo_pago;
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id_pedido` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `id_metodo_pago` int DEFAULT NULL,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
+  `id_direccion` int NOT NULL,
+  `gastos_envio` decimal(10,2) DEFAULT '0.00',
+  `codigo_seguimiento` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `estado` enum('pendiente','pagado','enviado','entregado','cancelado') COLLATE utf8mb4_unicode_ci DEFAULT 'pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Disparadores `pedido`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_audit_pedido_update` AFTER UPDATE ON `pedido` FOR EACH ROW BEGIN
+    INSERT INTO Auditoria(tabla_afectada,id_registro,operacion,datos_anteriores,datos_nuevos)
+    VALUES(
+        'Pedido',
+        NEW.id_pedido,
+        'UPDATE',
+        JSON_OBJECT('estado',OLD.estado,'total',OLD.total),
+        JSON_OBJECT('estado',NEW.estado,'total',NEW.total)
+    );
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `trg_historial_estado` AFTER UPDATE ON `pedido` FOR EACH ROW BEGIN
+    IF OLD.estado <> NEW.estado THEN
+        INSERT INTO Historial_Estado_Pedido(id_pedido, estado, fecha)
+        VALUES (NEW.id_pedido, NEW.estado, NOW());
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int NOT NULL,
+  `nombre` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci,
+  `id_categoria` int DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT '1',
+  `eliminado_en` datetime DEFAULT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int DEFAULT '0',
+  `imagen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Disparadores `producto`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_audit_producto_update` AFTER UPDATE ON `producto` FOR EACH ROW BEGIN
+    INSERT INTO Auditoria(tabla_afectada,id_registro,operacion,datos_anteriores,datos_nuevos)
+    VALUES(
+        'Producto',
+        NEW.id_producto,
+        'UPDATE',
+        JSON_OBJECT('precio',OLD.precio,'stock',OLD.stock,'activo',OLD.activo),
+        JSON_OBJECT('precio',NEW.precio,'stock',NEW.stock,'activo',NEW.activo)
+    );
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contrasena` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rol` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fecha_registro` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `auditoria`
+--
+ALTER TABLE `auditoria`
+  ADD PRIMARY KEY (`id_auditoria`);
+
+--
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD PRIMARY KEY (`id_carrito`),
+  ADD UNIQUE KEY `id_usuario` (`id_usuario`),
+  ADD KEY `idx_carrito_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `detalle_carrito`
+--
+ALTER TABLE `detalle_carrito`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `idx_detalle_carrito_carrito` (`id_carrito`),
+  ADD KEY `idx_detalle_carrito_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD PRIMARY KEY (`id_detalle_pedido`),
+  ADD KEY `idx_detalle_pedido_pedido` (`id_pedido`),
+  ADD KEY `idx_detalle_pedido_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD PRIMARY KEY (`id_direccion`),
+  ADD KEY `idx_direccion_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `favorito`
+--
+ALTER TABLE `favorito`
+  ADD PRIMARY KEY (`id_usuario`,`id_producto`),
+  ADD KEY `idx_favorito_usuario` (`id_usuario`),
+  ADD KEY `idx_favorito_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `historial_estado_pedido`
+--
+ALTER TABLE `historial_estado_pedido`
+  ADD PRIMARY KEY (`id_historial`),
+  ADD KEY `idx_historial_pedido` (`id_pedido`);
+
+--
+-- Indices de la tabla `metodo_pago`
+--
+ALTER TABLE `metodo_pago`
+  ADD PRIMARY KEY (`id_metodo_pago`),
+  ADD KEY `idx_metodo_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `idx_pedido_usuario` (`id_usuario`),
+  ADD KEY `idx_pedido_pago` (`id_metodo_pago`),
+  ADD KEY `idx_pedido_direccion` (`id_direccion`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `idx_producto_categoria` (`id_categoria`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `auditoria`
+--
+ALTER TABLE `auditoria`
+  MODIFY `id_auditoria` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  MODIFY `id_carrito` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_carrito`
+--
+ALTER TABLE `detalle_carrito`
+  MODIFY `id_detalle` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  MODIFY `id_detalle_pedido` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  MODIFY `id_direccion` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `historial_estado_pedido`
+--
+ALTER TABLE `historial_estado_pedido`
+  MODIFY `id_historial` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `metodo_pago`
+--
+ALTER TABLE `metodo_pago`
+  MODIFY `id_metodo_pago` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id_pedido` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD CONSTRAINT `carrito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_carrito`
+--
+ALTER TABLE `detalle_carrito`
+  ADD CONSTRAINT `detalle_carrito_ibfk_1` FOREIGN KEY (`id_carrito`) REFERENCES `carrito` (`id_carrito`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detalle_carrito_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
+
+--
+-- Filtros para la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
+
+--
+-- Filtros para la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD CONSTRAINT `direccion_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `favorito`
+--
+ALTER TABLE `favorito`
+  ADD CONSTRAINT `favorito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `favorito_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `historial_estado_pedido`
+--
+ALTER TABLE `historial_estado_pedido`
+  ADD CONSTRAINT `historial_estado_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `metodo_pago`
+--
+ALTER TABLE `metodo_pago`
+  ADD CONSTRAINT `metodo_pago_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`id_metodo_pago`) REFERENCES `metodo_pago` (`id_metodo_pago`),
+  ADD CONSTRAINT `pedido_ibfk_3` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id_direccion`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
