4b76759c83ebb6946e5776f195e437c3a926fb46
Elmaspro-web <ignaciolangarica68@gmail.com>
2026-02-27 15:28:43 +0100

    Feat: Añadir rutas de productos y categorías

--- tfg-backend/routes/api.php
+++ tfg-backend/routes/api.php
@@ -3,9 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/productos', [ProductoController::class, 'index']);
+Route::apiResource('productos', ProductoController::class);
+Route::get('productos/all', [ProductoController::class, 'indexAll']);
+Route::apiResource('categorias', CategoriaController::class);
+Route::patch('productos/{id}/toggle', [ProductoController::class, 'toggleActivo']);
+
+/*
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('productos', ProductoController::class)->only(['index', 'show']);
+    Route::apiResource('categorias', CategoriaController::class)->only(['index', 'show']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('productos', ProductoController::class)->except(['index', 'show']);
+    Route::apiResource('categorias', CategoriaController::class)->except(['index', 'show']);
+    Route::get('productos/all', [ProductoController::class, 'indexAll']);
+    Route::patch('productos/{id}/toggle', [ProductoController::class, 'toggleActivo']);
+});
+*/
