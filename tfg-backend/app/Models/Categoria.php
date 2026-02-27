28cf3a214312e4fc64a88742e57a3954d9c0a69b
Elmaspro-web <ignaciolangarica68@gmail.com>
2026-02-27 15:10:03 +0100

    Feat: Modelo Categoria

---
+++ tfg-backend/app/Models/Categoria.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = 'categoria';
+    protected $primaryKey = 'id_categoria';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion'
+    ];
+}
