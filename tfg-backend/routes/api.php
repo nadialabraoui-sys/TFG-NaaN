f69c7ec2cc07b322a3e5f6f9494b1acdc3f2a118
Elmaspro-web <ignaciolangarica68@gmail.com>
2026-03-01 14:28:55 +0100

    Feat: Protección de las rutas con auth:sanctum y el Middleware "admin"

--- tfg-backend/routes/api.php
+++ tfg-backend/routes/api.php
@@ -4,26 +4,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/registro', [AuthController::class, 'registro']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('productos', ProductoController::class);
-Route::get('productos/all', [ProductoController::class, 'indexAll']);
-Route::apiResource('categorias', CategoriaController::class);
-Route::patch('productos/{id}/toggle', [ProductoController::class, 'toggleActivo']);
+// Petición → auth:sanctum → no autenticado → /api/no-autenticado → JSON 401
+Route::get('/no-autenticado', function() {
+    return response()->json(['Error' => 'No autenticado'], 401);
+});
 
-/*
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('productos', ProductoController::class)->only(['index', 'show']);
     Route::apiResource('categorias', CategoriaController::class)->only(['index', 'show']);
 });
 
+// Petición → auth:sanctum → admin → CategoriaController
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::apiResource('productos', ProductoController::class)->except(['index', 'show']);
     Route::apiResource('categorias', CategoriaController::class)->except(['index', 'show']);
     Route::get('productos/all', [ProductoController::class, 'indexAll']);
     Route::patch('productos/{id}/toggle', [ProductoController::class, 'toggleActivo']);
 });
-*/
