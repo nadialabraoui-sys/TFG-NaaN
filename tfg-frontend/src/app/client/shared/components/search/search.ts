913616029dc04b7e0d8a34e550f83fb68a3187ce
nadialabraoui-sys <nadialabraoui@gmail.com>
2026-03-05 22:46:01 +0100

    añadido nuevo menu en header

--- tfg-frontend/src/app/client/shared/components/search/search.ts
+++ tfg-frontend/src/app/client/shared/components/search/search.ts
@@ -14,8 +14,32 @@ export class Search {
   query: string = "";
   mostrarBuscador: boolean = true;
   mostrarFiltros: boolean = false;
+  sesionIniciada: boolean = false;
+  nombreUsuario: string = "";
   tallas: string[] = ['XS', 'S', 'M', 'L', 'XL'];
   categorias: string[] = [];
+  divisas = [
+    { code: "USD", value: "US/$" },
+    { code: "EUR", value: "EU/€" },
+    { code: "GBP", value: "GB/£" },
+    { code: "JPY", value: "JP/¥" },
+    { code: "CNY", value: "CN/¥" },
+    { code: "KRW", value: "KR/₩" },
+    { code: "INR", value: "IN/₹" },
+    { code: "CAD", value: "CA/$" },
+    { code: "AUD", value: "AU/$" },
+    { code: "NZD", value: "NZ/$" },
+    { code: "CHF", value: "CH/CHF" },
+    { code: "SEK", value: "SE/kr" },
+    { code: "NOK", value: "NO/kr" },
+    { code: "DKK", value: "DK/kr" },
+    { code: "PLN", value: "PL/zł" },
+    { code: "CZK", value: "CZ/Kč" },
+    { code: "HUF", value: "HU/Ft" },
+    { code: "TRY", value: "TR/₺" },
+    { code: "AED", value: "AE/د.إ" },
+    { code: "SAR", value: "SA/﷼" }
+  ];
   minPrecio: number = 0;
   maxPrecio: number = Infinity;
 
